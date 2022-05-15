8e1a367cc960403ac02be9939774a3b2d455b9c1
wonya <wonya.dev@gmail.com>
2022-05-16 00:20:26 +0900

    add /items api

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/items")
+def read_root():
+    results = {
+        "01": "Hello",
+        "02": "World!"
+    }
+    return results
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
